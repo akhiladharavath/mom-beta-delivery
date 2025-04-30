be756d0fcccf6fac2be475ed457d176c4c2e631b
nizampet-rohith <rohith.nizam123@gmail.com>
2025-04-30 11:20:40 +0530

    updated

---
+++ app/profile/reusablebags.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function reusablebags() {
+  return (
+    <View>
+      <Text>reusablebags</Text>
+    </View>
+  )
+}
