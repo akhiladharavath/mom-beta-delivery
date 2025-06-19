5600b338ce4c472bf32debf29efccb05e108514f
Gnaneswar <gnaneswarkareti@gmail.com>
2025-06-19 11:40:23 +0530

    remove back function ality once order is accepted

--- app/delivery/_layout.tsx
+++ app/delivery/_layout.tsx
@@ -7,59 +7,45 @@ import { MaterialIcons } from '@expo/vector-icons'
 
 export default function _layout() {
   return (
-    <>
-    <Stack screenOptions={{
-      headerStyle:{
-        backgroundColor: COLORS.secondary
-      },
-      headerTitleAlign: 'center',
-    }}>
-      <Stack.Screen 
-        name='pickup' 
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.secondary,
+        },
+        headerTitleAlign: 'center',
+      }}
+    >
+      <Stack.Screen
+        name="pickup"
         options={{
           title: 'Pickup',
-          headerRight: () => <Help/>,
-          gestureEnabled:false,
-          // No back button on pickup screen
-        
-        //     <TouchableOpacity style={{ marginLeft: 15 }}>
-        //       <MaterialIcons name="arrow-back" size={24} color="transparent" />
-        //     </TouchableOpacity>
-        //   ),
-          
-        }} 
+          headerRight: () => <Help />,
+          headerLeft: () => null, 
+        }}
       />
-      <Stack.Screen 
-        name='deliver' 
+      <Stack.Screen
+        name="deliver"
         options={{
           title: 'Delivery',
-          headerRight: () => <Help/>,
+          headerRight: () => <Help />,
           
-            // <TouchableOpacity 
-            //   style={{ marginLeft: 15 }}
-            //   onPress={() => router.replace('/delivery/pickup')}
-            // >
-            //   <MaterialIcons name="arrow-back" size={24} color="#00a99d" />
-            // </TouchableOpacity>
-          
-        }} 
+        }}
       />
-      <Stack.Screen 
-        name='order' 
+      <Stack.Screen
+        name="order"
         options={{
           title: 'Order',
-          headerRight: () => <Help/>,
-          
-            // <TouchableOpacity 
-            //   style={{ marginLeft: 15 }}
-            //   onPress={() => router.replace('/delivery/deliver')}
-            // >
-            //   <MaterialIcons name="arrow-back" size={24} color="#00a99d" />
-            // </TouchableOpacity>
-          
-        }} 
+          headerRight: () => <Help />,
+          headerLeft: () => (
+            <TouchableOpacity
+              style={{ marginLeft: 15 }}
+              onPress={() => router.replace('/delivery/deliver')}
+            >
+              <MaterialIcons name="arrow-back" size={24} color="#00a99d" />
+            </TouchableOpacity>
+          ),
+        }}
       />
     </Stack>
-    </>
   )
 }
